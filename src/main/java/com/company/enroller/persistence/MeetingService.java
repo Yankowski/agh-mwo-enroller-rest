197cf4b5b134dc71013d25777a9be96541b8fadc
Jan Jankowski <jjankowski@novomatic-tech.com>
2018-05-16 20:06:35 +0200

    Update meeting rest controller
    1. Sorting meeting list by name of the meeting
    2. Searching meeting list by title or description
    3. Searching meeting list by registered meeting user

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -13,50 +13,49 @@ import com.company.enroller.model.Meeting;
 @Component("meetingService")
 public class MeetingService {
 
-	Session session;
+    Session session;
 
-	public MeetingService() {
-		session = DatabaseConnector.getInstance().getSession();
-	}
+    public MeetingService() {
+        session = DatabaseConnector.getInstance().getSession();
+    }
 
-	public Collection<Meeting> getAll() {
-		String hql = "FROM Meeting";
-		Query query = this.session.createQuery(hql);
-		return query.list();
-	}
+    public Collection<Meeting> getAll() {
+        String hql = "FROM Meeting";
+        Query query = this.session.createQuery(hql);
+        return query.list();
+    }
 
-	public Meeting findById(long id) {
-		Meeting meeting = (Meeting) this.session.get(Meeting.class, id);
-		if (meeting == null) {
-			throw new NoMeetingFoundException("No meeting with id '" + id + "' was found");
-		}
-		return meeting;
-	}
+    public Meeting findById(long id) {
+        Meeting meeting = (Meeting) this.session.get(Meeting.class, id);
+        if (meeting == null) {
+            throw new NoMeetingFoundException("No meeting with id '" + id + "' was found");
+        }
+        return meeting;
+    }
 
-	public void delete(Meeting meeting) {
-		Transaction transaction = this.session.beginTransaction();
-		this.session.delete(meeting);
-		transaction.commit();
-	}
+    public void delete(Meeting meeting) {
+        Transaction transaction = this.session.beginTransaction();
+        this.session.delete(meeting);
+        transaction.commit();
+    }
 
-	public void add(Meeting meeting) {
-		Transaction transaction = this.session.beginTransaction();
-		this.session.save(meeting);
-		transaction.commit();
-	}
+    public void add(Meeting meeting) {
+        Transaction transaction = this.session.beginTransaction();
+        this.session.save(meeting);
+        transaction.commit();
+    }
 
-	public void update(Meeting meeting) {
-		Transaction transaction = this.session.beginTransaction();
-		this.session.merge(meeting);
-		transaction.commit();
-	}
+    public void update(Meeting meeting) {
+        Transaction transaction = this.session.beginTransaction();
+        this.session.merge(meeting);
+        transaction.commit();
+    }
 
-	public boolean alreadyExist(Meeting meeting) {
-		String hql = "FROM Meeting WHERE title=:title AND date=:date";
-		Query query = this.session.createQuery(hql);
-		Collection resultList = query.setParameter("title", meeting.getTitle()).setParameter("date", meeting.getDate())
-				.list();
-		return query.list().size() != 0;
-	}
+    public boolean alreadyExist(Meeting meeting) {
+        String hql = "FROM Meeting WHERE title=:title AND date=:date";
+        Query query = this.session.createQuery(hql);
+        Collection resultList = query.setParameter("title", meeting.getTitle()).setParameter("date", meeting.getDate()).list();
+        return query.list().size() != 0;
+    }
 
 }
